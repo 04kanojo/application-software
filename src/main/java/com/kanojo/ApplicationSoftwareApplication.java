4b15096f41dc410e8118be2f93b1a7562ba27c33
HuTao <gongbo@hotmail.com>
2023-04-25 14:04:12 +0800

    初始化

---
+++ src/main/java/com/kanojo/ApplicationSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.kanojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanojo/ApplicationSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
