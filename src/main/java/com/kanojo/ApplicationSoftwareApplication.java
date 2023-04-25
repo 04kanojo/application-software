c4560109cd060b351eeb9c7b1401ae96e2fc7e22
HuTao <gongbo@hotmail.com>
2023-04-25 14:05:00 +0800

    初始化

--- src/main/java/com/kanojo/ApplicationSoftwareApplication.java
+++ src/main/java/com/kanojo/ApplicationSoftwareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationSoftwareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationSoftwareApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSoftwareApplication.class, args);
+    }
 
 }
